434620294ccef31185e3af1f8de1b9ade9a1c0a7
MaiconDev <haelyton149@gmail.com>
2025-08-03 23:24:07 -0300

    Ajusta classes que quebraram Atividade 7

--- src/main/java/Atividade7/Main.java
+++ src/main/java/Atividade7/Main.java
@@ -1,2 +1,13 @@
-package Atividade7;public class Main {
+package Atividade7;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Quarto quarto = new Quarto(101, "Suíte", "Disponível");
+        Hospede hospede = new Hospede("Carlos Silva", "12345678900");
+        Reserva reserva = new Reserva(quarto, hospede, LocalDate.of(2025, 8, 10), LocalDate.of(2025, 8, 15));
+
+        reserva.exibirDados();
+    }
 }
--- src/main/java/Atividade7/Reserva.java
+++ src/main/java/Atividade7/Reserva.java
@@ -1,2 +1,60 @@
-package Atividade7;public class Reserva {
+package Atividade7;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Reserva {
+    private Quarto quarto;
+    private Hospede hospede;
+    private LocalDate dataEntrada;
+    private LocalDate dataSaida;
+
+    public Reserva(Quarto quarto, Hospede hospede, LocalDate dataEntrada, LocalDate dataSaida) {
+        this.quarto = quarto;
+        this.hospede = hospede;
+        this.dataEntrada = dataEntrada;
+        this.dataSaida = dataSaida;
+    }
+
+    public void exibirDados() {
+        System.out.println("----- Dados da Reserva -----");
+        quarto.exibirDados();
+        hospede.exibirDados();
+
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        System.out.println("Data de Entrada: " + dataEntrada.format(formato));
+        System.out.println("Data de Saída: " + dataSaida.format(formato));
+    }
+
+    public LocalDate getDataEntrada() {
+        return dataEntrada;
+    }
+
+    public void setDataEntrada(final LocalDate dataEntrada) {
+        this.dataEntrada = dataEntrada;
+    }
+
+    public LocalDate getDataSaida() {
+        return dataSaida;
+    }
+
+    public void setDataSaida(final LocalDate dataSaida) {
+        this.dataSaida = dataSaida;
+    }
+
+    public Hospede getHospede() {
+        return hospede;
+    }
+
+    public void setHospede(final Hospede hospede) {
+        this.hospede = hospede;
+    }
+
+    public Quarto getQuarto() {
+        return quarto;
+    }
+
+    public void setQuarto(final Quarto quarto) {
+        this.quarto = quarto;
+    }
 }
