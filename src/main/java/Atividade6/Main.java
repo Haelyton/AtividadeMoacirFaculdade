24187a7a06aef25e7a976902ef4f9e88d9116a98
MaiconDev <haelyton149@gmail.com>
2025-08-03 23:19:11 -0300

    Atividade6 - Implementa Classe Curso

---
+++ src/main/java/Atividade6/Main.java
@@ -0,0 +1,9 @@
+package Atividade6;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso = new Curso("Engenharia de Software", "ENG123", 360);
+        curso.exibirDados();
+    }
+}
+
